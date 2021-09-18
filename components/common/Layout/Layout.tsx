d97786573fb7be0aacf6038165773824db917c26
Hayoung <gkduddy0814@gmail.com>
2021-09-19 01:52:31 +0900

    feat : login상태가 아니면 login페이지로 이동

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -2,8 +2,24 @@ import React from 'react';
 import s from './Layout.module.css';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectLogin } from 'lib/redux/login/loginSlice';
+import { useRouter } from 'next/dist/client/router';
+import { setUserData } from 'lib/redux/profile/profileSlice';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { login, name } = useSelector(selectLogin);
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    if (!login) {
+      router.replace('/login');
+    } else {
+      dispatch(setUserData(name));
+    }
+  }, [login, name, router]);
+
   return (
     <>
       <header>
--- components/profile/UserInfo/UserInfo.tsx
+++ components/profile/UserInfo/UserInfo.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import Link from 'next/link';
 import s from './UserInfo.module.css';
 
-import ProfileImage from '../ProfileImage';
-import Button from '@material-ui/core/Button';
-import SettingsIcon from '@material-ui/icons/Settings';
-
-import { formatNumber } from 'lib/common';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectProfile } from 'lib/redux/profile/profileSlice';
 import { setModal } from 'lib/redux/modal/modalSlice';
 
+import ProfileImage from '../ProfileImage';
+import Button from '@material-ui/core/Button';
+import SettingsIcon from '@material-ui/icons/Settings';
+
+import MoreHorizSharpIcon from '@material-ui/icons/MoreHorizSharp';
+
+import { formatNumber } from 'lib/common';
+import { selectLogin } from 'lib/redux/login/loginSlice';
+
 interface UserInfoProps {}
 
 const UserInfo: React.FC<UserInfoProps> = ({}) => {
+  const { login, name } = useSelector(selectLogin);
   const { userData } = useSelector(selectProfile);
   const dispatch = useDispatch();
 
@@ -24,27 +29,64 @@ const UserInfo: React.FC<UserInfoProps> = ({}) => {
           border={false}
           size={'profile'}
           imageUrl={userData.imageUrl}
+          story={'story'}
         />
         <div className={s.pcinfo}>
           <div className={s.tit01}>
             <div>{userData.id}</div>
-            <div>
-              <Link href="/accounts/edit">
-                <a>
-                  <Button size="small" variant="outlined">
-                    <b>프로필 편집</b>
-                  </Button>
+            {name === userData.id ? (
+              <>
+                <div>
+                  <Link href="/accounts/edit">
+                    <a>
+                      <Button size="small" variant="outlined">
+                        <b>프로필 편집</b>
+                      </Button>
+                    </a>
+                  </Link>
+                </div>
+                <a onClick={() => dispatch(setModal('setting', true))}>
+                  <SettingsIcon
+                    style={{ cursor: 'pointer' }}
+                    color="disabled"
+                    fontSize="medium"
+                  />
                 </a>
-              </Link>
-            </div>
-
-            <a onClick={() => dispatch(setModal('setting', true))}>
-              <SettingsIcon
-                style={{ cursor: 'pointer' }}
-                color="disabled"
-                fontSize="medium"
-              />
-            </a>
+              </>
+            ) : (
+              <>
+                {userData.follower.includes(name) ? (
+                  <>
+                    <span className={s.prs}>
+                      <Button size="small" variant="outlined">
+                        <b>메세지 보내기</b>
+                      </Button>
+                    </span>
+                    <span className={s.prl}>
+                      <Button size="small" variant="outlined">
+                        <b>팔로우 취소</b>
+                      </Button>
+                    </span>
+                  </>
+                ) : (
+                  <>
+                    <span className={s.prl}>
+                      <Button
+                        style={{
+                          backgroundColor: '#2294ff',
+                          color: 'white',
+                          width: '80px',
+                        }}
+                        size="small"
+                        variant="contained">
+                        <b>팔로우</b>
+                      </Button>
+                    </span>
+                  </>
+                )}
+                <MoreHorizSharpIcon style={{ fontSize: '26px' }} />
+              </>
+            )}
           </div>
           <div className={s.tit02}>
             <p>
@@ -52,12 +94,12 @@ const UserInfo: React.FC<UserInfoProps> = ({}) => {
             </p>
             <a>
               <p onClick={() => dispatch(setModal('followers', true))}>
-                팔로워 <span>{formatNumber(userData.follower)}</span>
+                팔로워 <span>{formatNumber(userData.follower.length)}</span>
               </p>
             </a>
             <a>
               <p onClick={() => dispatch(setModal('followings', true))}>
-                팔로우 <span>{formatNumber(userData.following)}</span>
+                팔로우 <span>{formatNumber(userData.following.length)}</span>
               </p>
             </a>
           </div>
@@ -101,13 +143,13 @@ const UserInfo: React.FC<UserInfoProps> = ({}) => {
           <div onClick={() => dispatch(setModal('followers', true))}>
             팔로워
           </div>
-          {formatNumber(userData.follower)}
+          {formatNumber(userData.follower.length)}
         </span>
         <span>
           <div onClick={() => dispatch(setModal('followings', true))}>
             팔로우
           </div>
-          {formatNumber(userData.following)}
+          {formatNumber(userData.following.length)}
         </span>
       </div>
     </>
