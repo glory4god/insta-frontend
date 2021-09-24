6a1c7ea242b4183f7770f238a1a9fd059b1f3cd7
Hayoung <gkduddy0814@gmail.com>
2021-09-24 10:38:30 +0900

    feat: modal
    modal data에 따른 변화

--- components/modal/Modal/Modal.tsx
+++ components/modal/Modal/Modal.tsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/dist/client/router';
 import s from '../CommonModal.module.css';
 
-import { selectModal, setModalInitial } from 'lib/redux/modal/modalSlice';
+import { selectProfile } from 'lib/redux/profile/profileSlice';
+import { setModalInitial } from 'lib/redux/modal/modalSlice';
 import { useDispatch, useSelector } from 'react-redux';
 
 import CloseSharpIcon from '@material-ui/icons/CloseSharp';
+import Button from '@material-ui/core/Button';
+
 import cn from 'classnames';
+import { ProfileImage } from 'components/profile';
+import { selectLogin } from 'lib/redux/login/loginSlice';
+import { ModalDataType } from 'types/modal/types';
 
 interface ModalProps {
-  title: ModalTitle[];
+  modalData: ModalDataType[];
 }
 
-type ModalTitle = {
-  name: string;
-  link: string | null;
-};
-
-const Modal: React.FC<ModalProps> = ({
-  title = [{ name: '팔로워', link: null }] as ModalTitle[],
-}) => {
-  const {} = useSelector(selectModal);
+const Modal: React.FC<ModalProps> = ({ modalData }) => {
+  const { id } = useSelector(selectLogin);
+  const { userData } = useSelector(selectProfile);
   const dispatch = useDispatch();
+  const router = useRouter();
 
   const closeModal = (
     e:
@@ -32,63 +34,170 @@ const Modal: React.FC<ModalProps> = ({
     dispatch(setModalInitial());
   };
 
+  const isMe: boolean = userData.id === id ? true : false;
+
+  const followerHandler = async (id: string, state: string) => {
+    // TODO: 팔로우하기/취소하기 api 기능 추가
+    if (state === 'follow') {
+    } else if (state === 'cancel') {
+    }
+  };
   return (
     <>
-      <div
-        className={s.outerContainer}
-        onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-          closeModal(e);
-        }}
-      />
-      <div className={s.absoluteModal}>
-        {title.length === 1 ? (
+      <div className={s.outerContainer_2} onClick={closeModal} />
+      <div className={s.innerContainer_2}>
+        {modalData.length === 1 ? (
           <>
-            {title?.map((arr, idx) => {
-              return (
-                <div className={s.header2_bt} key={arr.name + idx}>
-                  <Link href={`accounts/${arr.link}`}>
-                    <a>
-                      <p>
-                        <b>{arr.name}</b>
-                      </p>
-                    </a>
-                  </Link>
+            <div className={s.header2_bt}>
+              {modalData[0].link === undefined ? (
+                <div>
+                  <p>
+                    <b>{modalData[0].name}</b>
+                  </p>
                 </div>
-              );
-            })}
+              ) : (
+                <Link href={modalData[0].link}>
+                  <a>
+                    <p>
+                      <b>{modalData[0].name}</b>
+                    </p>
+                  </a>
+                </Link>
+              )}
+            </div>
             <CloseSharpIcon
-              onClick={(e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
-                e.preventDefault();
-                closeModal(e);
-              }}
+              onClick={closeModal}
               className={s.out2}
               fontSize="large"
             />
+            {/* TODO: 테스트 데이터 만든 후에 방법적용 (팔로워 누를 때 모달)*/}
+            {modalData[0].name === '팔로워' && (
+              <>
+                {userData.follower.map((p, idx) => {
+                  return (
+                    <div
+                      key={userData.id + 'follower' + idx}
+                      className={s.content2}>
+                      <div>
+                        <ProfileImage size={'board'} imageUrl={p.imageUrl} />
+                        <div>
+                          <span>
+                            <b>{p.id}</b>{' '}
+                            {isMe && (
+                              <span style={{ color: '#2294ff' }}>
+                                · <b>팔로우</b>
+                              </span>
+                            )}
+                          </span>
+                          <span style={{ color: 'rgb(140,140,140)' }}>
+                            {p.id}
+                          </span>
+                        </div>
+                      </div>
+                      {/*TODO: 내 상태알 때, 나랑 팔로워 상태인지 아닌지에 따른 결과보여주는 로직 짜야함*/}
+                      {isMe ? (
+                        <Button
+                          size="small"
+                          variant="outlined"
+                          style={{
+                            height: '28px',
+                          }}
+                          onClick={() => followerHandler(p.id, '')}>
+                          <b>삭제</b>
+                        </Button>
+                      ) : ['winter', 'irene'].includes(p.id) ? (
+                        <Button
+                          size="small"
+                          variant="outlined"
+                          style={{
+                            height: '28px',
+                          }}
+                          onClick={() => followerHandler(p.id, '')}>
+                          <b>팔로잉</b>
+                        </Button>
+                      ) : (
+                        <Button
+                          size="small"
+                          style={{
+                            height: '28px',
+                            color: 'white',
+                            backgroundColor: '#2294ff',
+                          }}
+                          onClick={() => followerHandler(p.id, '')}>
+                          <b>팔로우</b>
+                        </Button>
+                      )}
+                    </div>
+                  );
+                })}
+              </>
+            )}
+            {/* 팔로우 누를 때 모달 */}
+            {modalData[0].name === '팔로우' && (
+              <>
+                {userData.following.map((p, idx) => {
+                  return (
+                    <div
+                      key={userData.id + 'follwing' + idx}
+                      className={s.content2}>
+                      <div>
+                        <ProfileImage size={'board'} imageUrl={p.imageUrl} />
+                        <span>{p.id}</span>
+                      </div>
+                      {/*TODO: 내 상태알 때, 나랑 팔로우 상태인지 아닌지에 따른 결과보여주는 로직 짜야함*/}
+                      {['winter', 'irene'].includes(p.id) ? (
+                        <Button
+                          size="small"
+                          variant="outlined"
+                          style={{
+                            height: '28px',
+                          }}
+                          onClick={() => followerHandler(p.id, '')}>
+                          <b>팔로잉</b>
+                        </Button>
+                      ) : (
+                        <Button
+                          size="small"
+                          style={{
+                            height: '28px',
+                            color: 'white',
+                            backgroundColor: '#2294ff',
+                          }}
+                          onClick={() => followerHandler(p.id, '')}>
+                          <b>팔로우</b>
+                        </Button>
+                      )}
+                    </div>
+                  );
+                })}
+              </>
+            )}
           </>
         ) : (
           <>
-            {title.map((arr, idx) => {
+            {modalData.map((arr, idx) => {
               return (
                 <div
                   className={cn(
-                    title.length - 1 !== idx ? s.header2_bt : s.header2,
+                    modalData.length - 1 !== idx ? s.header2_bt : s.header2,
                   )}
                   style={{ fontSize: '14px' }}
-                  key={arr.name + idx}>
-                  <Link href={`accounts/${arr.link}`}>
-                    <a>
-                      <p>{arr.name}</p>
-                    </a>
-                  </Link>
+                  key={arr.name + idx}
+                  onClick={arr.onClick}>
+                  <a
+                    style={{ color: arr.color }}
+                    onClick={() => {
+                      if (arr.link !== undefined) {
+                        router.replace(arr.link);
+                      }
+                    }}>
+                    <p>{arr.name}</p>
+                  </a>
                 </div>
               );
             })}
           </>
         )}
-        {}
-
-        {/* TODO: 테스트 데이터 만든 후에 방법적용 */}
-        <div></div>
       </div>
     </>
   );
