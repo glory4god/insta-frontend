3fde014071eb967bb7f7bd73575518b6e0753785
Hayoung <gkduddy0814@gmail.com>
2021-09-24 10:38:55 +0900

    feat : signup types

---
+++ types/signup/types.ts
@@ -0,0 +1,33 @@
+export type EmailProps = {
+  email: string;
+  setEmail: Function;
+}
+
+export type PhonenumberProps = {
+  phonenumber: string;
+  setPhonenumber: Function;
+}
+
+export type SignupProps = {
+  id: string;
+  setId: Function;
+  name: string;
+  setName: Function;
+  username: string;
+  setUsername: Function;
+  password: string;
+  setPassword: Function;
+  setEmail: Function;
+  setPhonenumber: Function;
+}
+
+export interface IFormInputs {
+  id: string;
+  name: string;
+  username: string;
+  password: string;
+}
+
+export interface ICodeInputs {
+  certificationCode?: string;
+}
