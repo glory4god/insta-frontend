8413b714ca8bce81d5703e84b5bff62c456d23ef
Hayoung <gkduddy0814@gmail.com>
2021-10-11 01:06:31 +0900

    feat : add local server

--- next.config.js
+++ next.config.js
@@ -5,4 +5,10 @@ module.exports = {
     config.resolve.modules.push(__dirname); // 추가
     return config;
   },
+
+  env: {
+    AWS_SERVER:
+      'http://ec2-3-34-110-210.ap-northeast-2.compute.amazonaws.com:8080',
+    LOCAL_SERVER: 'http://localhost:3000/api/v1',
+  },
 };
